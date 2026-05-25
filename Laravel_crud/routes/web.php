e6f88b5946c95fc960414591494c5b7e6cfe0657
sebastiangzq <xgth300@gmail.com>
2026-05-25 00:26:56 -0400

    Delete Laravel_crud directory

--- Laravel_crud/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\SucursalController;
-use Illuminate\Support\Facades\Route;
-
-Route::redirect('/', '/sucursals');
-
-Route::resource('sucursals', SucursalController::class);
